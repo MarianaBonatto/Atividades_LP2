47b5da24927aab726fd1a6dcfb8ce8afbf61a9c0
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-07-20 16:20:38 -0300

    Add files via upload

---
+++ Lista02_LP2/src/lista02_lp2/Exercicio02.java
@@ -0,0 +1,21 @@
+package lista02_lp2;
+public class Exercicio02 
+{
+    /* Classe FileInputStream :
+        - método read()
+            public abstract int read(){}
+                ELE É RESPONSÁVEL POR LER O BYTE NA ENTRADA
+        - método readbyte()
+            public int read(byte[] b){}
+               LÊ ALGUM NÚMERO DE BYTES DO FLUXO DE ENTRADA E ARMAZENA EM UMA MATRIZ
+    
+       Classe FileOutputStream
+        - método  write()
+            public void write(){}
+                ELE É RESPONSÁVEL POR GRAVAR O BYTE NO ARQUIVO
+        - método writebyte()
+            public void write(byte[] b){}
+               GRAVA OS BYTES E ARMAZENA EM UMA MATRIZ
+    */
+    
+}
---
+++ Lista02_LP2/src/lista02_lp2/Exercicio03.java
@@ -0,0 +1,19 @@
+package lista02_lp2;
+public class Exercicio03 
+{
+    /*
+        FileNotFileExeption 
+            - Sinaliza que uma tentativa de abrir o arquivo indicado por um nome 
+              de caminho especificado falhou.
+    
+            EOFExeption 
+            - Sinaliza que um fim de arquivo ou fim de fluxo foi atingido 
+              inesperadamente durante a entrada.
+    
+            IOExeption 
+            - Sinaliza que ocorreu uma exceção de E / S (Entrada e Saída) de 
+              algum tipo. Esta classe é a classe geral de exceções produzida por
+              operações de E / S com falha ou interrompidas.
+    */
+    
+}
