4ee7df38921d44d90ee5ac9b20d92ef233219d4e
Mariana Bonatto <71667434+MarianaBonatto@users.noreply.github.com>
2021-09-30 14:14:49 -0300

    Add files via upload

---
+++ Lista01_LP2/src/lista01_lp2/Exercicio05.java
@@ -0,0 +1,42 @@
+package lista01_lp2;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Exercicio05 
+{
+    public static void main (String[]args)
+    {
+        Scanner teclado = new Scanner(System.in);
+        String palavra;
+        HashSet<String> set = new HashSet<String>();
+        
+        do
+        {
+        System.out.println("Digite palavras ou fim para encerrar : ");
+        palavra = teclado.next();
+        
+        if(! palavra.equals("fim"))
+          {
+            set.add(palavra);
+          }
+        }
+        while(! palavra.equals("fim"));
+        
+        do
+        {
+            System.out.println("Novamente, digite mais palavras ou fim para encerrar o programa : ");
+            palavra = teclado.next();
+            
+            if(! palavra.equals("fim"))
+            {
+                if(set.contains(palavra))
+                    System.out.println(palavra + " -" + " Localizado");
+                else
+                    System.out.println(palavra + " -" + " Não Localizado");
+            }
+        } 
+        while(! palavra.equals("fim"));
+        System.out.println("Progarama Encerrado");
+    }
+}
